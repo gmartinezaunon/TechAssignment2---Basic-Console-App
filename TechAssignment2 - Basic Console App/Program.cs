a0fdbff0e2d69bc5cbb58f0a5ab7b06718fd7d96
Gonzalo Martinez-Aunon <gonzalo8@usf.edu>
2022-01-27 15:56:55 -0500

    Commit

--- TechAssignment2 - Basic Console App/Program.cs
+++ TechAssignment2 - Basic Console App/Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
-
+// Programmer: Gonzalo Martinez Aunon
 namespace TechAssignment2___Basic_Console_App
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("If you're not first you're last is a great quote by an American Hero Ricky Bobby.");
         }
     }
 }
